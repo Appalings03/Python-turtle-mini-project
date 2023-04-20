c6092c0fd1188a049ec34e5a9a6b8fc9d365bab5
Martin Binamé <marti>
2023-04-20 11:48:37 +0200

    Operators introduction

---
+++ Indroduction/operators.py
@@ -0,0 +1,10 @@
+healt = 100
+damage = 20
+healt = healt - damage
+print(healt)
+
+healt = healt + 50
+print(healt)
+healt = healt*1.5
+healt = healt/2
+print(healt)
--- Logs.md
+++ Logs.md
@@ -1,2 +1,7 @@
-20/04 Introduction / Creating Variables / Operators
+20/04 Introduction 
 
+20/04 Creating Variables
+
+20/04 Operators
+
+20/04 Condition
