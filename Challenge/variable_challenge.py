f39386674fcb0d81b2400a3bcaa3de348f372dae
Martin Binamé <marti>
2023-04-20 11:14:04 +0200

    Adding variable integration challenge

---
+++ Challenge/variable_challenge.py
@@ -0,0 +1,11 @@
+# Creating 4 variables
+
+country_name = "Belgium"
+population = 11_590_000
+landlocked = True
+border_size = 556 + 133 + 130 + 478
+
+print(country_name)
+print(population)
+print(landlocked)
+print(border_size)
