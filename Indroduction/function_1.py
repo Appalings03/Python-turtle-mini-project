6aee7859024fcc7bf0b73fca2aa897d003a65b66
Martin Binamé <marti>
2023-04-25 14:32:11 +0200

    add basics function definition

---
+++ Indroduction/function_1.py
@@ -0,0 +1,13 @@
+
+def say_hello(name):
+    print("Hello " + name)
+
+name =["Bob", "Martin", "Merlin"]
+say_hello("Martin")
+
+def add_numbers (a, b):
+    sum = a + b
+    return sum
+num = add_numbers(150, 2063)
+
+print(num)
---
+++ Indroduction/function_2.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+def move_and_turn(distance,angle):
+    forward(distance)
+    right(angle)
+
+
+move_and_turn(100,45)
+move_and_turn(100,45)
+move_and_turn(100,45)
+move_and_turn(100,45)
+move_and_turn(100,45)
+move_and_turn(100,45)
+move_and_turn(100,45)
+move_and_turn(100,45)
+done()
