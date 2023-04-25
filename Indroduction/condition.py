ed190151a02bd1cfabc1a3a523107ad4f4319053
Martin Binamé <marti>
2023-04-25 13:59:24 +0200

    condition

---
+++ Indroduction/condition.py
@@ -0,0 +1,29 @@
+a = 5
+b = 5
+
+if a == b:
+  print("a is equal to b")
+
+if a > b:
+  print("a is greater than b")
+
+if a < b:
+  print("a is less than b")
+  
+if a != b:
+  print("a doesn't equal b")
+  
+if a >= b:
+  print("a is greater than or equals to b")
+
+if a <= b:
+  print("a is less than or equals to b")
+
+
+temperature = 60
+target_temp = 40
+
+if temperature < target_temp:
+  print("Too hot!")
+elif temperature > target_temp:
+  print("Too cold!")
